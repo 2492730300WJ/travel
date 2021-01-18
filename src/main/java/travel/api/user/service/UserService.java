ac5a1d2a5c0714728860a469055dd04d10dbf9c7
汪健 <2492730300@qq.com>
2021-01-18 10:44:23 +0800

    msg:User 重构 ; person:wj;

--- src/main/java/travel/api/user/service/UserService.java
+++ src/main/java/travel/api/user/service/UserService.java
@@ -1,79 +1,11 @@
 package travel.api.user.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.apache.commons.lang.StringUtils;
-import travel.api.config.jwt.TokenService;
-import travel.api.config.session.SessionInfo;
-import travel.api.config.response.WorkException;
-import travel.api.config.response.WorkStatus;
-import travel.api.table.entity.SysUserInfo;
-import travel.api.table.mapper.SysUserInfoMapper;
 import travel.api.user.entity.User;
-import travel.api.util.RedisUtil;
 import net.sf.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
+public interface UserService {
 
-/**
- * @Author: dd
- */
-@Service
-public class UserService {
+    public JSONObject login(User user);
 
-    @Autowired
-    private SysUserInfoMapper sysUserInfoMapper;
-
-    @Autowired
-    private RedisUtil redisUtil;
-
-    @Autowired
-    TokenService tokenService;
-
-    public JSONObject login(User user) {
-        JSONObject jsonObject = new JSONObject();
-        SysUserInfo userInfo = null;
-        // 手机号登录
-        if (null != user.getPhone()) {
-            userInfo = sysUserInfoMapper.selectOne(new QueryWrapper<SysUserInfo>().eq("phone", user.getPhone()));
-            if (userInfo == null) {
-                throw new WorkException(WorkStatus.PASSWORD_IS_ERROR);
-            }
-        }
-        if (StringUtils.isNotBlank(user.getUserCard())) {
-            userInfo = sysUserInfoMapper.selectOne(new QueryWrapper<SysUserInfo>().eq("user_card", user.getUserCard()).eq("user_password", user.getPassword()));
-            if (userInfo == null) {
-                throw new WorkException(WorkStatus.PASSWORD_IS_ERROR);
-            }
-        }
-
-        // 存入session
-        SessionInfo sessionInfo = new SessionInfo();
-        sessionInfo.setUserId(userInfo.getUserId());
-        sessionInfo.setUserName(userInfo.getUserName());
-        sessionInfo.setUserCard(userInfo.getUserCard());
-        sessionInfo.setAvatar(userInfo.getAvatar());
-
-        // 登陆时获取长Token和短Token
-        String token = tokenService.getToken(sessionInfo);
-        String refreshToken = tokenService.getRefreshToken(sessionInfo);
-        jsonObject.put("token", token);
-        jsonObject.put("refreshToken", refreshToken);
-        // 存入redis
-        redisUtil.set("token" + userInfo.getUserId(), token);
-        redisUtil.set("refreshToken" + userInfo.getUserId(), refreshToken);
-        jsonObject.put("user", sessionInfo);
-        return jsonObject;
-    }
-
-    public JSONObject info(User user) {
-        if (null == user.getUserId()) {
-            throw new WorkException(WorkStatus.CHECK_PARAM);
-        }
-        SysUserInfo userInfo = sysUserInfoMapper.selectOne(new QueryWrapper<SysUserInfo>().eq("user_id", user.getUserId()));
-        JSONObject jsonObject = new JSONObject();
-        user.setPassword(null);
-        jsonObject.put("user",userInfo);
-        return jsonObject;
-    }
+    public JSONObject info(User user);
 }
---
+++ src/main/java/travel/api/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,82 @@
+package travel.api.user.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.apache.commons.lang.StringUtils;
+import travel.api.config.jwt.TokenService;
+import travel.api.config.session.SessionInfo;
+import travel.api.config.response.WorkException;
+import travel.api.config.response.WorkStatus;
+import travel.api.table.entity.SysUserInfo;
+import travel.api.table.mapper.SysUserInfoMapper;
+import travel.api.user.entity.User;
+import travel.api.user.service.UserService;
+import travel.api.util.RedisUtil;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @Author: dd
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private SysUserInfoMapper sysUserInfoMapper;
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Autowired
+    TokenService tokenService;
+
+    @Override
+    public JSONObject login(User user) {
+        JSONObject jsonObject = new JSONObject();
+        SysUserInfo userInfo = null;
+        // 手机号登录
+        if (null != user.getPhone()) {
+            userInfo = sysUserInfoMapper.selectOne(new QueryWrapper<SysUserInfo>().eq("phone", user.getPhone()));
+            if (userInfo == null) {
+                throw new WorkException(WorkStatus.PASSWORD_IS_ERROR);
+            }
+        }
+        if (StringUtils.isNotBlank(user.getUserCard())) {
+            userInfo = sysUserInfoMapper.selectOne(new QueryWrapper<SysUserInfo>().eq("user_card", user.getUserCard()).eq("user_password", user.getPassword()));
+            if (userInfo == null) {
+                throw new WorkException(WorkStatus.PASSWORD_IS_ERROR);
+            }
+        }
+
+        // 存入session
+        SessionInfo sessionInfo = new SessionInfo();
+        sessionInfo.setUserId(userInfo.getUserId());
+        sessionInfo.setUserName(userInfo.getUserName());
+        sessionInfo.setUserCard(userInfo.getUserCard());
+        sessionInfo.setAvatar(userInfo.getAvatar());
+
+        // 登陆时获取长Token和短Token
+        String token = tokenService.getToken(sessionInfo);
+        String refreshToken = tokenService.getRefreshToken(sessionInfo);
+        jsonObject.put("token", token);
+        jsonObject.put("refreshToken", refreshToken);
+        // 存入redis
+        redisUtil.set("token" + userInfo.getUserId(), token);
+        redisUtil.set("refreshToken" + userInfo.getUserId(), refreshToken);
+        jsonObject.put("user", sessionInfo);
+        return jsonObject;
+    }
+
+    @Override
+    public JSONObject info(User user) {
+        if (null == user.getUserId()) {
+            throw new WorkException(WorkStatus.CHECK_PARAM);
+        }
+        SysUserInfo userInfo = sysUserInfoMapper.selectOne(new QueryWrapper<SysUserInfo>().eq("user_id", user.getUserId()));
+        JSONObject jsonObject = new JSONObject();
+        user.setPassword(null);
+        jsonObject.put("user",userInfo);
+        return jsonObject;
+    }
+}
