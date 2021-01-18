f5821e221b5b6a7821c1b73322ba0d546ac8ae23
汪健 <2492730300@qq.com>
2021-01-18 13:59:51 +0800

    msg:DELETE CODE ; person:wj;

--- src/main/java/travel/api/user/service/UserService.java
+++ src/main/java/travel/api/user/service/UserService.java
@@ -3,6 +3,9 @@ package travel.api.user.service;
 import travel.api.user.entity.User;
 import net.sf.json.JSONObject;
 
+/**
+ * @author Administrator
+ */
 public interface UserService {
 
     public JSONObject login(User user);
