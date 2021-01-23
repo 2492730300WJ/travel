273a6269b13c754f1d34f2b94258312921a267f6
汪健 <2492730300@qq.com>
2021-01-23 17:37:46 +0800

    msg:msg ; person:wj;

--- src/main/java/travel/api/ApiApplication.java
+++ src/main/java/travel/api/ApiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+
+
 /**
  * @author Administrator
  */
@@ -12,7 +14,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan(basePackages = {"travel.api.config"})
 @MapperScan(basePackages = {"travel.api.table.mapper"})
 public class ApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
