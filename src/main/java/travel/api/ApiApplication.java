b7c70c5c087e25a1e09f6981f37760b855db7f6c
汪健 <2492730300@qq.com>
2021-02-04 16:47:09 +0800

    msg:nacos ; person:wj;

--- src/main/java/travel/api/ApiApplication.java
+++ src/main/java/travel/api/ApiApplication.java
@@ -1,5 +1,11 @@
 package travel.api;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,16 +21,26 @@ import java.util.TimeZone;
 @SpringBootApplication
 @ServletComponentScan(basePackages = {"travel.api.config"})
 @MapperScan(basePackages = {"travel.api.table.mapper"})
-public class ApiApplication {
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
+@EnableNacosDiscovery(globalProperties = @NacosProperties(serverAddr = "47.102.121.70:8848"))
+public class ApiApplication{
 
     @PostConstruct
     void setDefaultTimezone() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
     }
 
+    @NacosInjected
+    private NamingService namingService;
+
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @PostConstruct
+    public void registerInstance() throws NacosException {
+        namingService.registerInstance("api", "47.102.121.70", 9999);
     }
 
 }
